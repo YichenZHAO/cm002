82f2c1be618f680a350ea2be286716b33a1330ad
YichenZHAO <zhaoyc86@hotmail.com>
2014-09-15 17:48:45 -0700

    cm002_Sep08

---
+++ cm002.R
@@ -0,0 +1,29 @@
+#sequence
+seq(from=1, to=10)
+1:10
+
+a<-2
+
+#creating a string (text argument)
+yo  <- "hello world"
+yo
+yoo <- 'hello world'
+yoo
+(yo <- "hello world")
+(yoo<-'hello world')
+
+#display date
+date()
+
+ls()
+objects() #show objects
+rm(yo) #remove relected object
+rm(list=ls()) #remove all objects
+
+#get working directory
+getwd()
+
+#create an Rstudio project
+#File -> New Project -> New Directory
+#quit R
+q()
